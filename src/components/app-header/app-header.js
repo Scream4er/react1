5a191bf85fae5415c773e6901695bb1ca89a32f2
ScreamKO <Chaykov.Artem1@gmail.com>
2023-01-21 19:36:58 +0100

    Components + styles + states

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './app-header.css';
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Artem Chaikovskyi</h1>
+            <h2>5 posts, 1 with like</h2>
+        </div>
+    );
+};
+
+export default AppHeader;
